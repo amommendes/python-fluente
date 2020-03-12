bd86640019f0b80b3e425ae75c5ac23713237517
Amom Luiz <amomluiz@SPF1LT-PJ000000.ldap.quintoandar.com.br>
2020-03-12 17:37:40 -0300

    Playing with inheritance

---
+++ playing/playing.py
@@ -0,0 +1,27 @@
+class Animal():
+    
+    def __init__(self, name="Unicorn"):
+        self.name = name
+    
+    def eat(self):
+        return "I'm can eat anything"
+
+class Dog(Animal):
+    def __init__(self, name = None):
+        super().__init__()
+        self.superName = self.name
+        self.InstanceDogName = name
+        
+    def sound(self):
+        print("Au, au! is the sound that %s (my superClass Name or %s to my friends) produces" %  (self.superName, self.InstanceDogName ))
+    
+    def eat(self):
+        return "As a superAnimal I could eat anything. But as a Dog I can eat just eat somethings" 
+
+        
+
+if __name__ == "__main__":
+    animal= Animal("ted")
+    dog = Dog("Spike")
+    print("My name is %s and %s" % (dog.name, dog.eat() ))
+    dog.sound()
